0308605f37e45c85e10a1569448b71add0ea0ce5
NIKI443 <140906614+NIKI443@users.noreply.github.com>
2023-09-23 18:12:35 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,81 +1,31 @@
-
-const input1 = document.getElementById ('input1')
-const submit = document.getElementById ('input2')
-const plusBtn = document.getElementById ('plus')
-const minusBtn = document.getElementById ('minus')
-const multiplytBtn = document.getElementById ('multiply')
-const dividetBtn = document.getElementById ('divide')
-const submitBtn = document.getElementById ('submit')
-const obnuleBtn = document.getElementById ('obnule')
-let action = '+'
-let masive = [0]
+const display = document.querySelector("#input1");
+const submitBtn = document.querySelector("#submit");
+const btns = document.querySelectorAll(".js-btn");
+const obnuleBtn = document.querySelector(".obnule");
 
 obnuleBtn.onclick = function() {
-  location.reload(); 
-  return false;
-}
-
-plusBtn.onclick = function () {
-    action = '+'
-    calculations()
-  }
-  
-  minusBtn.onclick = function () {
-    action = '-'
-    calculations()
+    return display.value = ""
   }
 
-  multiplytBtn.onclick = function () {
-    action = '*'
-    calculations()
-  }
-  
-  dividetBtn.onclick = function () {
-    action = '/'
-    calculations()
-  }
+btns.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    let itemText = e.target.textContent;
+    if (itemText === "*") {
+      itemText = "*";
+    }
+    if (itemText === "/") {
+      itemText = "/";
+    }
+    if(itemText !=="="){
+    display.value += itemText;
+    }
+    if (itemText === "=") {
+        display.value = eval(display.value)
+      }
 
-  function  printResult(result){
-    submit.textContent = result 
-    return input1.value = ""
-  }
-  
- 
+    
+  });
+});
 
-  
-  function calculations() {
-  const nam1 = masive[masive.length-1]
-  if (action == '+') {
-        const pluse = nam1 + Number(input1.value)
-         masive.push(pluse)
-    return nam1
-    }else if (action == '-') {
-      const minuse = nam1 - Number(input1.value)
-       masive.push(minuse)
-      return  nam1  
-    }else if (action == '*') {
-      const multiplye = nam1 * Number(input1.value)
-      masive.push(multiplye)
-      return nam1
-
-    }else if (action == '/') {
-      const dividee = nam1 / Number(input1.value)
-      masive.push(dividee)
-      return nam1
-     }
-  }
-
-
-
-
-
-
-
-submitBtn.onclick = function(){
-    const result = +masive[masive.length-1].toFixed(6)
-    printResult(result)
-   
-
-}
 
 
